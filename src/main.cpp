b8d96e98119f7a648f56588636007e2606b1bfc0
rra.roro@gmail.com <rra.roro@gmail.com>
2019-05-19 22:35:47 +0300

    Bug Fix

--- src/main.cpp
+++ src/main.cpp
@@ -67,9 +67,9 @@ int main(int argc, char* argv[])
                   diagonal_matrix[i][9 - i] = 9 - i;
             }
 
-            for (int i = 0; i < 10; ++i)
+            for (int i = 1; i < 9; ++i)
             {
-                  for (int j = 0; j < 10; ++j)
+                  for (int j = 1; j < 9; ++j)
                   {
                         cout << diagonal_matrix[i][j] << " ";
                   }
